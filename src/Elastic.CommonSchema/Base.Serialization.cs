667014eed797e60df7398fd366df5b8d36e16c73
Martijn Laarman <Mpdreamz@gmail.com>
2019-11-28 12:53:39 +0100

    stage commit

--- src/Elastic.CommonSchema/Base.Serialization.cs
+++ src/Elastic.CommonSchema/Base.Serialization.cs
@@ -2,15 +2,43 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 using Elastic.CommonSchema.Serialization;
 using Utf8Json;
 using Utf8Json.Resolvers;
+using JsonSerializer = Utf8Json.JsonSerializer;
 
 namespace Elastic.CommonSchema
 {
 	[JsonFormatter(typeof(BaseJsonFormatter))]
 	public partial class Base
 	{
-		public byte[] Serialize() => JsonSerializer.Serialize(this, StandardResolver.ExcludeNull);
+		private static JsonSerializerOptions SerializerOptions { get; } =
+			new JsonSerializerOptions { IgnoreNullValues = true, WriteIndented = false, };
+
+		public byte[] Serialize() => System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(this, SerializerOptions);
+		public Task SerializeAsync(Stream s) => System.Text.Json.JsonSerializer.SerializeAsync(s, this, SerializerOptions);
+		public void Serialize(Stream s)
+		{
+			using var writer = new Utf8JsonWriter(s);
+			System.Text.Json.JsonSerializer.Serialize(writer, this, SerializerOptions);
+		}
+	}
+
+	public class BaseJsonConverter : JsonConverter<Base>
+	{
+		public override Base Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+
+		}
+
+		public override void Write(Utf8JsonWriter writer, Base value, JsonSerializerOptions options)
+		{
+
+		}
 	}
 }
